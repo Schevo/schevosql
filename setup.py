e6945c6dc9aaa9b71c01894b266186041cf02a61
Matthew R. Scott <gldnspud@gmail.com>
2006-10-26 16:31:01 +0000

    Remove usage of finddata

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import sys, os
 import textwrap
 
-import finddata
+## import finddata
 
 setup(
     name="SchevoSql",
@@ -47,7 +47,7 @@ setup(
 
     packages=find_packages(exclude=['doc', 'tests']),
 
-    package_data=finddata.find_package_data(),
+##     package_data=finddata.find_package_data(),
 
     zip_safe=False,
     
